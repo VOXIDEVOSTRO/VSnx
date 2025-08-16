f6224312181f06df51cf6f452d31d04f3228ae1d
VOXIDEVOSTRO <adityabansal2010@outlook.com>
2025-08-16 21:41:01 +0530

    Update u_mem.c

--- os/kernel/u_mem.c
+++ os/kernel/u_mem.c
@@ -1,5 +1,28 @@
 #include "include.h"
 
+/*
+MIT License
+
+Copyright (c) 2025 Aditya Bansal
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom 
+the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 // Initialize allocation tracking
 void init_user_tracking (void) {
     if (user_tracking_initialized) return;
@@ -499,3 +522,4 @@ void user_stack_free(void* stack_top, size_t size) {
     void* stack_base = (uint8_t*)stack_top - size + 16;
     user_free(stack_base);
 }
+
