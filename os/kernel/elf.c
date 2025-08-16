d568c28571de9089ed69fd358397e29a0b720dec
VOXIDEVOSTRO <adityabansal2010@outlook.com>
2025-08-16 21:36:04 +0530

    Update elf.c

--- os/kernel/elf.c
+++ os/kernel/elf.c
@@ -1,5 +1,28 @@
 #include "include.h"
 
+/*
+MIT License
+
+Copyright (c) 2025 Aditya Bansal
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom 
+the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /*==============================================================================================================
   ELF THREAD LOADER - COMPLETE ELF EXECUTION IN THREADS
 ================================================================================================================*/
@@ -648,3 +671,4 @@ uint32_t elf_thread_loader(const char* elf_path, const char* thread_name) {
     
     return thread_id;
 }
+
